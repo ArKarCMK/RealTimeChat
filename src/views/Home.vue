d7e3de5f54f70728db951719262dc1db24786807
ArKar <arkarchanmyaeko@gmail.com>
2023-07-30 15:28:27 +0630

    style: added global style

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container home">
+    Let's start!
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .home {
+    text-align: center;
+    padding: 20px 0;
+  }
+</style>
